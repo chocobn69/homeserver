2f51ebcfaeebb4549c1f4d23944bd87ce9137069
chocobn69 <choco.bn@gmail.com>
2018-12-21 12:41:24 +0100

    Torrent docker run script

--- torrent/run_backup.sh
+++ torrent/run_backup.sh
@@ -7,15 +7,16 @@ docker rm -f torrent
 docker run --cap-add=NET_ADMIN --device=/dev/net/tun -d \
               -v /home/choco/datas/:/data \
               -v /etc/localtime:/etc/localtime:ro \
-              -e OPENVPN_PROVIDER=XXX \
-              -e OPENVPN_CONFIG=XXX \
-              -e OPENVPN_USERNAME=XXX \
-              -e OPENVPN_PASSWORD=XXX \
+              -e OPENVPN_PROVIDER= \
+              -e OPENVPN_CONFIG= \
+              -e OPENVPN_USERNAME= \
+              -e OPENVPN_PASSWORD= \
+	      -e OPENVPN_OPTS= \
               -e WEBPROXY_ENABLED=false \
 	      -e PUID=$(id -u) \
 	      -e PGID=$(id -g) \
               -e LOCAL_NETWORK=192.168.1.0/24 \
-	      --dns 8.8.8.8 --dns 8.8.4.4 \
+	      --dns 1.1.1.1 --dns 8.8.8.8 \
               --log-driver json-file \
               --log-opt max-size=10m \
 	      --name torrent \
